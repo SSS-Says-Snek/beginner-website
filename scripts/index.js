7698f83a895b725006f32607a051996164df17b4
Brandon <bmcomis2018@gmail.com>
2022-07-04 23:38:31 -0500

    Add bad fade-in anim and gallery

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
 let num_clicks = 0;
+let amogus_gallery_idx = 0;
+let gallery_idxs = {};
+let gallery_files = {};
 
 
 const click_measurer = document.getElementById("click-measurer");
@@ -11,3 +14,23 @@ click_measurer.addEventListener('click', () => {
         click_measurer.textContent = `You clicked me ${num_clicks} times!`;
     }
 });
+
+
+const galleries = document.getElementsByClassName("gallery");
+for (const gallery of galleries) {
+    let file_info = gallery.id.split(",");
+
+    gallery_idxs[gallery] = 0;
+    gallery_files[gallery] = {"filename": file_info[0], "num_files": parseInt(file_info[1])};
+
+    gallery.addEventListener('click', () => {
+        gallery_idxs[gallery]++;
+        gallery_idxs[gallery] %= gallery_files[gallery]["num_files"];
+
+        let filename = gallery_files[gallery]["filename"];
+        let gallery_idx = gallery_idxs[gallery];
+        
+        gallery.setAttribute("src", `imgs/${filename}_${gallery_idx + 1}.png`);
+        gallery.setAttribute("width", gallery.getAttribute("width"));
+    })
+}
